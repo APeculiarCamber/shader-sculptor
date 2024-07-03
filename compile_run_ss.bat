339d28ec4726552b89aaad7a35a02dc47e283194
AnEvenStrangerCamber <jay.idema@gmail.com>
2024-07-02 23:39:02 -0400

    Finished the build reshuffle

--- compile_run_ss.bat
+++
@@ -1,2 +0,0 @@
- g++ -g -std=c++11 -I./include -L./lib ./src/main.cpp ./src/imgui/*.cpp .\src\graphics\*.cpp ./src/math/*.cpp ./src/ss/*.cpp ./src/glad.c -lglfw3dll -o ./shader_sculptor.exe
- shader_sculptor.exe
