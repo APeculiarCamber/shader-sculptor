a86b70feda07e000d19597f136c3162c9b2b57f3
AnEvenStrangerCamber <jay.idema@gmail.com>
2024-07-13 09:37:49 -0400

    Include reorder (shifting includes to be before declarations in main)

--- src/main.cpp
+++ src/main.cpp
@@ -119,7 +119,6 @@ int main()
 
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
 void ProcessInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -127,10 +126,7 @@ void ProcessInput(GLFWwindow *window)
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
 void FramebufferSizeCallback(GLFWwindow* window, int width, int height)
 {
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
