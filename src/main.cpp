a2e15b1f56396faced7dbcbb27fa550de3956098
AnEvenStrangerCamber <jay.idema@gmail.com>
2024-07-13 09:36:44 -0400

    Include reorder

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <algorithm>
 #include <vector>
 #include <iostream>
 
@@ -8,7 +7,6 @@
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
 #include "ss/ss_graph.hpp"
-#include "graphics/ga_material.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
